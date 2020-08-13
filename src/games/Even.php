6c5eb54b0f62452e7f2c074662c4dbee3a9975d3
dinikoff <zarazablin@gmail.com>
2020-08-13 16:38:11 +0300

    step 6

--- src/games/Even.php
+++ src/games/Even.php
@@ -5,22 +5,23 @@ namespace Brain\Games\Even;
 use function cli\line;
 use function cli\prompt;
 
+
 function run()
 {
     line('Welcome to the Brain Game!');
     line('Answer "yes" if the number is even, otherwise answer "no".');
     line(' ');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line('Hello, %s!', $name);
     line(' ');
 
     $numberOfCorrectAnswers = 0;
     while ($numberOfCorrectAnswers !== 3) {
         $number = rand(1, 99);
         line("Question: {$number}");
-        $userAnswer = strtolower(trim(prompt("Your answer")));
+        $userAnswer = strtolower(trim(prompt('Your answer')));
         if (($userAnswer === 'yes' && isEven($number)) || ($userAnswer === 'no' && !isEven($number))) {
-            line("Correct!");
+            line('Correct!');
             $numberOfCorrectAnswers++;
             if ($numberOfCorrectAnswers === 3) {
                 line("Congratulations, {$name}!");
@@ -32,17 +33,23 @@ function run()
             break;
         }
     }
-}
+
+}//end run()
+
 
 function isEven($number)
 {
-    return $number % 2 === 0;
-}
+    return ($number % 2) === 0;
+
+}//end isEven()
+
 
 function getCorrectAnswer($number)
 {
     if (isEven($number)) {
-        return "yes";
+        return 'yes';
     }
-    return "no";
-}
+
+    return 'no';
+
+}//end getCorrectAnswer()
