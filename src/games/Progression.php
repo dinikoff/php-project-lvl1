c74c6bcd25fb14aaa9f5be1d20954eb1a2aa95c2
dinikoff <zarazablin@gmail.com>
2020-09-03 00:24:24 +0300

    refactor Progression.php

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -9,26 +9,23 @@ const TASK = 'What number is missing in the progression?';
 function generatorQuestionAnswer()
 {
     $progressionLength = 10;
-    $currentNumber = rand(1, 99);
+    $firstNumber = rand(1, 99);
     $step = rand(1, 10);
     $hiddenIndex = rand(0, $progressionLength - 1);
-    $answer = null;
-    $question = null;
-    $parts = [];
-    for ($j = 0; $j < $progressionLength; $j++) {
-        if ($j === $hiddenIndex) {
-            $parts[$j] = '..';
-            $answer = strval($currentNumber);
-        } else {
-            $parts[$j] = $currentNumber;
-        }
-        $currentNumber += $step;
-    }
-    $question = implode(' ', $parts);
+    $progression = generateProgression($firstNumber, $step, $progressionLength);
+    $answer = strval($progression[$hiddenIndex]);
+    $progression[$hiddenIndex] = '..';
+    $question = implode(' ', $progression);
     return ['question' => $question, 'answer' => $answer];
 }
 
 function runProgression()
 {
     runGame(fn() => generatorQuestionAnswer(), TASK);
+}
+
+function generateProgression($firstElement, $step, $progressionLength)
+{
+    $lastElement = $firstElement + $step * ($progressionLength - 1);
+    return range($firstElement, $lastElement, $step);
 }
